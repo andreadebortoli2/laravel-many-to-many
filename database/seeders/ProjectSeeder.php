a88f7d8c45a90b38f69ee82dd1f468c21eeddd6d
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-20 18:18:50 +0200

    add project model, migration and seeder

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Project;
+use Faker\Generator as Faker;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+
+        for ($i = 0; $i < 20; $i++) {
+
+            $project = new Project();
+            $project->title = $faker->sentence();
+            $project->author = $faker->userName();
+            $project->description = $faker->text();
+            $project->image = $faker->imageUrl(700, 550, null, true, null, false, 'png');
+            $project->save();
+        }
+    }
+}
