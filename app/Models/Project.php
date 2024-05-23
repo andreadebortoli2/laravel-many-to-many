a32e2625f8dd4baf3d9308b4a6f8ae7895fa364a
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-05-23 15:36:17 +0200

    add connection between Type and Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
     protected $fillable = ['title', 'slug', 'author', 'source_code_url', 'production_site_url', 'image', 'description'];
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
